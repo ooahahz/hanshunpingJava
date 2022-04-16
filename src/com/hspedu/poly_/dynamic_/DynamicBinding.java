83087b9dfbf1862d099544720e1f4dbd575dd02f
haohao <z1446722092@gmail.com>
2022-04-16 15:56:34 +0800

    change comments

--- src/com/hspedu/poly_/dynamic_/DynamicBinding.java
+++ src/com/hspedu/poly_/dynamic_/DynamicBinding.java
@@ -3,7 +3,7 @@ package com.hspedu.poly_.dynamic_;
 public class DynamicBinding {
     public static void main(String[] args) {
         A a = new B(); // 向上转型
-        System.out.println(a.sum()); // 40
-        System.out.println(a.sum1()); // 30
+        System.out.println(a.sum()); // 40 -> 30
+        System.out.println(a.sum1()); // 30 -> 20
     }
 }
