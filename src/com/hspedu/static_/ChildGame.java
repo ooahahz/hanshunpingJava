7b203ff0d0a3d81179f654cb7d393b3da9389488
haohao <z1446722092@gmail.com>
2022-04-20 18:19:22 +0800

    add some notifications of static variable

--- src/com/hspedu/static_/ChildGame.java
+++ src/com/hspedu/static_/ChildGame.java
@@ -24,6 +24,11 @@ class Child {
     private String name;
     public static int count = 0; // 定义一个变量 count，是一个类变量（静态变量） static 静态
     // 该变量最大的特点就是会被 Child 类的所有对象实例共享
+    // 在进行类加载的时候，每一个类都会对应一个 Class 实例
+    // static 变量保存在 Class 实例的尾部，Class 对象在堆中。
+    // JDK7 以上的版本，静态域存储于定义类型的 Class 对象中，Class 对象如同堆中其他对象一样，存在于 GC 堆中
+    // Mark: 1. static 变量是所有对象（同一个类）所共享的
+    //       2. static 变量实在类加载的时候就生成了
 
     public Child(String name) {
         this.name = name;
