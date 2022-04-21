f282084b7f33cc60a4d834df3adda337c4fffa85
haohao <z1446722092@gmail.com>
2022-04-21 11:13:17 +0800

    add codeblock contents

---
+++ src/com/hspedu/static_/StaticExercise02.java
@@ -0,0 +1,25 @@
+package com.hspedu.static_;
+
+public class StaticExercise02 {
+    public static void main(String[] args) {
+        System.out.println("Number of total is " + Person1.getTotalPerson());
+        Person1 p1 = new Person1();
+        System.out.println("Number of total is " + Person1.getTotalPerson());
+    }
+
+}
+
+class Person1 {
+    public int id;
+    public static int total = 0;
+
+    public static int getTotalPerson() {
+//        id++;
+        return total;
+    }
+
+    public Person1() {
+        total++;
+        id = total;
+    }
+}
